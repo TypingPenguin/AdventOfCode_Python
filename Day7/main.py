a6a3cecf6dd09e256f8bd540ee5449026b2e28fc
Floris Kromwijk <floris.kromwijk@gmail.com>
2022-12-10 23:03:45 +0100

    Day7_update

--- Day7/main.py
+++ Day7/main.py
@@ -1,10 +1,34 @@
+fileSizeDict = {}
+
+dict = {Name: "Parent",
+        child:{
+            name
+        }
+        }
 def main():
     stringInput = readFile("input1.txt")
-    print (stringInput)
+    for line in stringInput:
+        if (line.__contains__("cd..")):
+            do_something()
+        elif (line.__contains__("cd /")):
+            do_something()
+        elif (line.__contains__("cd")):
+            do_something()
+        elif (line.__contains__("dir")):
+            do_something()
+        elif (line.__contains__("ls")):
+            do_something()
+        else:
+            line = line.split()
+            fileSizeDict.update({line[1]:line[0]})
 
+fileSizeDict.update({"something":"sasfdasd"})
+fileSizeDict.update({"something":"sasfddasd"})
 
+print(fileSizeDict.get("something"))
 
-
+def do_something():
+    pass
 def readFile(inputFileName):
     file = open(inputFileName, 'r')
     lines = file.readlines()
