70c3d45e0273a8b3873a60c1a0c0a60fa61a35e0
TypingPenguin <floris.kromwijk@gmail.com>
2022-12-13 01:54:38 +0100

    Day 12 array input

--- Day12/main.py
+++ Day12/main.py
@@ -1,8 +1,56 @@
 
+startCoord = []
+endCoord = []
+
 def main():
-    stringInput = readFile("input.txt")
+    stringInput = readFile("input1.txt")
     print(stringInput[0])
 
+
+    arrayInput = getArrayInput(stringInput)
+    arrayInput[startCoord[0]][startCoord[1]] = 0
+    arrayInput[endCoord[0]][endCoord[1]] = 26
+
+
+    for i in arrayInput:
+        for x in i:
+            print("%3d" %(x), end= "")
+        print("")
+    print()
+    print(startCoord)
+    print(endCoord)
+
+
+
+
+def getArrayInput(stringInput):
+    global startCoord
+    global endCoord
+
+    lineCounter = 0
+    treeArray = []
+
+    for line in stringInput:
+        listLine = list(line)
+        lineArray = []
+        charCounter = 0
+        for i in listLine:
+            if i.__contains__("\n"):
+                pass
+            else:
+                lineArray.append(ord(i)-97)
+
+            #Get start and end coordinates
+            if i.__contains__("S"):
+                startCoord = [lineCounter, charCounter]
+
+            if i.__contains__("E"):
+                endCoord = [lineCounter, charCounter]
+
+            charCounter += 1
+        treeArray.append(lineArray)
+        lineCounter += 1
+    return treeArray
 
 
 def readFile(inputFileName):
